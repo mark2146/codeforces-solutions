8e0670c14020672628d85e5be6d4c3378a7b3244
mark798 <104543712+mark5269@users.noreply.github.com>
2023-11-08 14:11:49 +0800

    Add files via upload

---
+++ jdoodle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int x;
+    cin>>x;
+    int y=x-2;
+    if(y%2 || y==0){
+        cout<<"NO"<<endl;
+    }else{
+        cout<<"YES"<<endl;
+    }
+}
